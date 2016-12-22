2852410bc24be088a7a39b7b8b116bb4373d4834
Jérôme Navez <jrm.mrj1@gmail.com>
2016-12-22 08:50:30 +0100

    Update bot.sh

--- Test_scripts/bot.sh
+++ Test_scripts/bot.sh
@@ -3,7 +3,6 @@ GLOBAL_URL=http://$(cat ../install/CNAME)
 URLEVENT=$(curl -X POST -Ls -o /dev/null -w %{url_effective} $GLOBAL_URL)
 
 date
-echo $URLEVENT
 START=$(date +%s)
 curl \
 -F "file=@test.zip" \
